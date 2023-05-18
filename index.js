54da303a220995553eef142745ea18d99ffd5e56
tajkir-alam <tajkir.alam@gmail.com>
2023-05-18 23:37:15 +0600

    mongo connection build

--- index.js
+++ index.js
@@ -8,6 +8,46 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
+
+// TrovTitans
+// OlzIpl43mniy9ZDW
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://TrovTitans:OlzIpl43mniy9ZDW@cluster0.h2ziqne.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const ShopByCategory = client.db("TrovTitans").collection("ShopByCategory");
+
+        app.get('/shopbycategory', async (req, res) => {
+            const cursor = ShopByCategory.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('server is working')
 })
