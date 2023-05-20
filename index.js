11ec3ee05cc943d1f455cb02f7f94dbe3a017789
tajkir-alam <tajkir.alam@gmail.com>
2023-05-20 23:45:22 +0600

    toys add

--- index.js
+++ index.js
@@ -76,6 +76,12 @@ async function run() {
             res.send(result)
         })
 
+        app.post('/alltoys', async (req, res) => {
+            const allToys = req.body;
+            const result = await ShopByCategory.insertOne(allToys);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
