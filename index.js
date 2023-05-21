a0909ab276b4d089def7f1eeb56e9dfbd9b8d263
tajkir-alam <tajkir.alam@gmail.com>
2023-05-21 20:20:32 +0600

    delete operation added

--- index.js
+++ index.js
@@ -119,6 +119,13 @@ async function run() {
         })
 
 
+        app.delete('/alltoys/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const result = await ShopByCategory.deleteOne(filter);
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
